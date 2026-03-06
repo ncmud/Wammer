0b192164c47d7ae3e66642e0b54a69b2b0f43460
James Power <jep@lepoer.us>
2026-03-06 00:38:21 -0500

    Install Tuist via mise in Xcode Cloud post-clone script.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-brew install tuist
+curl https://mise.jdx.dev/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+mise install tuist
+eval "$(mise activate bash)"
+
 cd "$CI_PRIMARY_REPOSITORY_PATH"
 tuist generate --no-open
