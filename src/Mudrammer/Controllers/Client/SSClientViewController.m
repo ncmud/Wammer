d9204c4c10e7b9e06c3dc5d89f8f49f8e1806c9f
James Power <jep@lepoer.us>
2026-03-06 23:01:12 -0500

    Remove dead MRMenuClearScreen notification observer.

--- src/Mudrammer/Controllers/Client/SSClientViewController.m
+++ src/Mudrammer/Controllers/Client/SSClientViewController.m
@@ -148,12 +148,6 @@ typedef void (^SPLSettingsCloseBlock) (void);
                                                      name:WorldStoreBridge.didChangeNotification
                                                    object:nil];
 
-#if TARGET_OS_MACCATALYST
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(menuClearScreen:)
-                                                     name:@"MRMenuClearScreen"
-                                                   object:nil];
-#endif
 
         // font changes cause NAWS
         for (NSString *observed in kObservedProperties) {
@@ -741,12 +735,6 @@ typedef void (^SPLSettingsCloseBlock) (void);
     [self.writeQueue cancelAllOperations];
     [self.mudView clearText];
 }
-
-#if TARGET_OS_MACCATALYST
-- (void)menuClearScreen:(NSNotification *)note {
-    [self clearText];
-}
-#endif
 
 #pragma mark - MFMailComposeViewControllerDelegate
 
