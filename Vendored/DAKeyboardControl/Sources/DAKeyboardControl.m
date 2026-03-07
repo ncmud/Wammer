54a60e0b6b1f7d1220f499b0408128fb2f5d848c
James Power <jep@lepoer.us>
2026-03-07 00:01:31 -0500

    Remove DAKeyboardControl addSubview: swizzle that conflicts with UIKit keyboard views.

--- Vendored/DAKeyboardControl/Sources/DAKeyboardControl.m
+++ Vendored/DAKeyboardControl/Sources/DAKeyboardControl.m
@@ -40,24 +40,10 @@ static char UIViewKeyboardOpened;
 @implementation UIView (DAKeyboardControl)
 @dynamic keyboardTriggerOffset;
 
-+ (void)load
-{
-    // Swizzle the 'addSubview:' method to ensure that all input fields
-    // have a valid inputAccessoryView upon addition to the view heirarchy
-    SEL originalSelector = @selector(addSubview:);
-    SEL swizzledSelector = @selector(swizzled_addSubview:);
-    Method originalMethod = class_getInstanceMethod(self, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
-    class_addMethod(self,
-					originalSelector,
-					class_getMethodImplementation(self, originalSelector),
-					method_getTypeEncoding(originalMethod));
-	class_addMethod(self,
-					swizzledSelector,
-					class_getMethodImplementation(self, swizzledSelector),
-					method_getTypeEncoding(swizzledMethod));
-    method_exchangeImplementations(originalMethod, swizzledMethod);
-}
+// +load swizzle removed: injecting inputAccessoryView on every addSubview:
+// conflicts with UIKit's internal keyboard views on iOS 26+, causing
+// unsatisfiable constraints. The app sets inputAccessoryView explicitly
+// in SSMUDToolbar, and responderDidBecomeActive: handles the fallback.
 
 #pragma mark - Public Methods
 
@@ -550,32 +536,6 @@ static char UIViewKeyboardOpened;
     return found;
 }
 
-- (void)swizzled_addSubview:(UIView *)subview
-{
-    if (!subview.inputAccessoryView)
-    {
-        if ([subview isKindOfClass:[UITextField class]])
-        {
-            UITextField *textField = (UITextField *)subview;
-            if ([textField respondsToSelector:@selector(setInputAccessoryView:)])
-            {
-                UIView *nullView = [[UIView alloc] initWithFrame:CGRectZero];
-                nullView.backgroundColor = [UIColor clearColor];
-                textField.inputAccessoryView = nullView;
-            }
-        }
-        else if ([subview isKindOfClass:[UITextView class]]) {
-            UITextView *textView = (UITextView *)subview;
-            if ([textView respondsToSelector:@selector(setInputAccessoryView:)] && [textView respondsToSelector:@selector(isEditable)] && textView.isEditable)
-            {
-                UIView *nullView = [[UIView alloc] initWithFrame:CGRectZero];
-                nullView.backgroundColor = [UIColor clearColor];
-                textView.inputAccessoryView = nullView;
-            }
-        }
-    }
-    [self swizzled_addSubview:subview];
-}
 
 #pragma mark - Property Methods
 
